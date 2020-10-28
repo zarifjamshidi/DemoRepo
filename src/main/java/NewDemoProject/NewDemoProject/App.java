b0b060079ecaf9b85a6f64ec3213b0e1231fa250
zarifjamshidi <zakej55@gmail.com>
2020-10-28 14:53:53 -0400

    this is demo commit

---
+++ src/main/java/NewDemoProject/NewDemoProject/App.java
@@ -0,0 +1,13 @@
+package NewDemoProject.NewDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
